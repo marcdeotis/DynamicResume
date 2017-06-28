f4e8e0b9df7c4ad2ac67c9b0b85cbe9ae3946516
Marc DeOtis <marc@deotis.com>
2017-06-28 15:52:27 -0500

    Moving away from specific titles to data Leaving it open to setting titles within XML attribute and data is styled based on element used

--- Scripts/resume.js
+++ Scripts/resume.js
@@ -9,22 +9,47 @@ $(document).ready(function () {
         dataType: 'xml',
         success: function (xml) {
             UpdateUserData(xml)
+            $xml = xml;
         }
+    });
 
+    $('.resume').on('click', '#Header>.Header-Item', null, function () {
+
+        $('#Header').children().show();
+        $(this).hide();
+        $('#Details').children().hide();
+
+        console.log($(this).attr('data-index'));
+
+        $('#Details [data-index=' + $(this).attr('data-index') + ']').show();
+
+    });
+
+    $('.resume').on('mouseenter', '#Header>.Header-Item', null, function () {
+        $(this).css({
+            'cursor': 'zoom-in'
+        })
+    });
+
+    $('.resume').on('mouseleave', '#Header>.Header-Item', null, function () {
+        $(this).css({
+            'cursor': ''
+        })
     });
 });
 
 
 function UpdateUserData(xml) {
-
+    //name
     $('.name').text($(xml).find('Name').text());
+    //social networks
     $('#a-linkedin')
         .attr('href', 'https://www.linkedin.com/in/' + ($(xml).find('Linkedin-user').text()))
-        .text($(xml).find('Linkedin').text());
+        .text($(xml).find('Linkedin-user').text());
 
     $('#a-github')
         .attr('href', 'https://www.github.com/' + ($(xml).find('Github-user').text()))
-        .text($(xml).find('Github').text());
+        .text($(xml).find('Github-user').text());
 
     $('#a-email')
         .attr('href', 'mailto: ' + ($(xml).find('Email').text()))
@@ -32,16 +57,68 @@ function UpdateUserData(xml) {
 
     $('#phone').text($(xml).find('Phone').text());
 
-    $(xml).find('Certifications>item').each(function (index, item) {
-        $('#certification-area>ul').append($('<li>').text($(item).text()))
-    })
+    //stuff???
+
+    $(xml)
+        .find('[SectionTitle]')
+        .each(function (index, header) {
+
+            $('#Header').append($('<div>')
+                .attr({'class': 'Header-Item', 'data-index': index})
+                .text($(header).attr('SectionTitle')));
+
+            console.log($(header).prop('tagName'));
+
+            if ($(header).prop('tagName') == 'SingleColList'
+                || $(header).prop('tagName') == 'MultiColList') {
+
+                if ($(header).prop('tagName') == 'SingleColList') {
+                    var template = $('#single-col-list').clone().html();
+                } 
+                else if ($(header).prop('tagName') == 'MultiColList') {
+                    var template = $('#multi-col-list').clone().html();
+                }
+
+                var html = $.parseHTML(template);
+                $(html).attr('data-index', index);
+                var itemTemplate = $(html).find('.item').clone();
+                
+                $(html).find('.item').remove();
+
+                $(html).find('.title').text($(header).attr('SectionTitle'));
+
+                $(header).find('item').each(function (index, item) {
+                    $(html).append($(itemTemplate).text($(item).text()));
+                });
+
+                $('#Details').append(html);        
+            }
+
+        });
 
 
-    $(xml).find('TechnicalSkills>item')
-        .each(function (index, item) {
-            $('#technical-skills-area>ul')
-                .append($('<li>')
-                .attr('class', 'col-md-4')
-                .text($(item).text()))
-    })
+
+
+    //$(xml).find('TechnicalSkills>item')
+    //    .each(function (index, item) {
+    //        $('#technical-skills-area>ul')
+    //            .append($('<li>')
+    //            .attr('class', 'col-md-4')
+    //            .text($(item).text()))
+    //    });
+
+    //console.log($(xml).find('Experiences').attr('SectionTitle'));
+
+    //$(xml).find('Experiences>Experience')
+    //    .each(function (index, experience) {
+    //        console.log($(experience).find('Company').text());
+            
+    //        $(experience)
+    //            .find('item')
+    //            .each(function (index, item) {
+    //                console.log($(item).text())
+    //            });
+    //    });
+
+
 };
